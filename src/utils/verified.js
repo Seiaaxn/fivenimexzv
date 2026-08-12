98ae446c6e783f8bb15868cb8124aeb4dae5a69f
Shin <ryu694602@gmail.com>
2026-08-12 11:09:47 +0700

    Update verified.js

--- src/utils/verified.js
+++ src/utils/verified.js
@@ -1,4 +1,4 @@
-export const VERIFIED_EMAIL = 'seiiyvn5@gmail.com';
+export const VERIFIED_EMAIL = 'ryu694602@gmail.com';
 
 export const isVerifiedEmail = (email) =>
   !!email && email.toLowerCase() === VERIFIED_EMAIL;
